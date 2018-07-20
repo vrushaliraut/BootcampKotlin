6f89250574c3529cc1b58d50ec3e2f09e0a5e87a
Vrushali <vrushali.raut@go-jek.com>
2018-07-20 18:02:32 +0530

    Work on Kotlin Pairs And Triples operators

--- app/src/main/java/bootcamp/com/bootcampkotlin/MainActivity.kt
+++ app/src/main/java/bootcamp/com/bootcampkotlin/MainActivity.kt
@@ -1,12 +1,38 @@
 package bootcamp.com.bootcampkotlin
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val book = Book("The Phoenix Project", "Bill", 2001)
+
+        val bookTitleAuthor = book.getTitleAuthor()
+
+        val bookTitleAuthorYear = book.getTitleAuthorAndYear()
+
+        Log.d("Pair Example","Here is your book ${bookTitleAuthor.first} by ${bookTitleAuthor.second}" )
+
+        Log.d("Triples Example ","Here is your book ${bookTitleAuthorYear.first} by" +
+                " ${bookTitleAuthorYear.second} written in ${bookTitleAuthorYear.third}")
+
     }
+
+    class Book(val title : String, val author : String, val year : Int){
+
+        fun getTitleAuthor() :Pair<String,String>{
+            return title to author
+        }
+
+        fun getTitleAuthorAndYear() : Triple<String, String, Int>{
+            return Triple(title,author,year)
+        }
+    }
+
+
 }
