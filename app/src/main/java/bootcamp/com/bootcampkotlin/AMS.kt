07b4f0e6e4d0aae1604d1b31452f0ffa10ae5852
Vrushali <vrushali.raut@go-jek.com>
2018-07-20 17:03:35 +0530

    Work on Kotlin labdas, and Hogh order functions

--- app/src/main/java/bootcamp/com/bootcampkotlin/AMS.kt
+++ app/src/main/java/bootcamp/com/bootcampkotlin/AMS.kt
@@ -15,8 +15,54 @@ class AMS : AppCompatActivity() {
 }
 
 fun main(){
+    //Lambdas
+    lamdaExample()
+    lamdaExample2()
     feedTheFish()
     eagerExample()
+}
+
+fun lamdaExample2() {
+   val numbers = listOf(1..12)
+   val rollDice = { sides : Int -> numbers[Random().nextInt(12)] }
+
+   val rollDice0 = { sides: Int ->
+        if (sides == 0) 0
+        else Random().nextInt(sides) + 1
+    }
+    val rollDice2: (Int) -> Int = { sides ->
+        if (sides == 0) 0
+        else Random().nextInt(sides) + 1
+    }
+
+    gamePlay1(rollDice)
+    gamePlay(rollDice2(4))
+}
+
+fun gamePlay(rollDice2: Int) {
+    println(rollDice2)
+}
+
+fun gamePlay1(rollDice: (Int) -> IntRange) {
+    println(rollDice)
+}
+
+
+/*Lamdas  - an expressions which declares functions
+* anonymous Functions
+* Function Literals
+*
+* Kotlin prefers Function argument to e last parameter*/
+fun lamdaExample() {
+    swim()
+}
+
+val swim = {Log.d("","Smimmm")}
+var dirty = 20
+val waterFilter: (Int) -> Int = { dirty : Int -> dirty / 2 }
+fun feedFish(dirty: Int) = dirty + 10
+fun updateDirty(dirty: Int, operation : (Int) -> Int) : Int{
+    return operation(dirty)
 }
 
 fun eagerExample() {
@@ -53,8 +99,18 @@ fun feedTheFish() {
     if (shouldChangeWeather(day)){
         Log.d("","Change the water today")
     }
+    //Call Dirty processor
+    dirtyProcessor()
+}
 
+fun dirtyProcessor() {
+    dirty = updateDirty(dirty, waterFilter)
 
+    //Feedfish is function and not a lambda so
+    dirty = updateDirty(dirty, ::feedFish)
+
+    //Using this syntax we can write down function which look like built in
+    dirty = updateDirty(dirty, { dirty -> dirty + 50 })
 }
 
 fun shouldChangeWeather(day: String, temperature: Int = 22, dirty: Int = getDirtySensorReading()): Boolean {
