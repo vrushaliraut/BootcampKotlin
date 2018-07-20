17bf4887d4ac2d2051601dc0a836bf0e09896e82
Vrushali <vrushali.raut@go-jek.com>
2018-07-20 14:13:51 +0530

    Work on Kotlin filters Operaters

--- app/src/main/java/bootcamp/com/bootcampkotlin/AMS.kt
+++ app/src/main/java/bootcamp/com/bootcampkotlin/AMS.kt
@@ -1,22 +1,57 @@
 package bootcamp.com.bootcampkotlin
 
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import java.util.*
 
-fun main(args : Array<String>){
-    feedTheFish()
+class AMS : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        main()
+    }
 }
+
+fun main(){
+    feedTheFish()
+    eagerExample()
+}
+
+fun eagerExample() {
+    val decorations = listOf("Plates","Paan", "abs", "pp", "paper")
+    val eager = decorations.filter { it[0] == 'p' }
+    Log.d("eager: ","$eager")
+
+    //apply fiter lazily
+    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+    Log.d("filtered: ","$filtered")
+    Log.d("filtered list: ","$filtered.toList()")
+
+    //Apply map lazily
+    val lazyMap = decorations.asSequence().map {
+        Log.d("Map: ","$it")
+    }
+
+    Log.d("lazyMap: ", "$lazyMap")
+    Log.d("first:"," ${lazyMap.first()}")
+    Log.d("first:"," ${lazyMap.toList()}")
+
+}
+
 
 fun feedTheFish() {
     val day = randomDay()
     val food = fishFood(day)
-    println("Today is $day and fish eat $food")
+    Log.d("","Today is $day and fish eat $food")
 
     shouldChangeWeather(day, 20 , 50)
     shouldChangeWeather(day)
     shouldChangeWeather(day,dirty = 50)
 
     if (shouldChangeWeather(day)){
-        println("Change the water today")
+        Log.d("","Change the water today")
     }
 
 
@@ -38,6 +73,8 @@ fun makeNewAuarium() = println("Building new aquarium..")
 fun aquariumStatusReport(aquarium : Any = makeNewAuarium()){
 
 }
+
+/*Single ' ' -> character , Double " " -> Strings */
 
 //Compact function :- Simplify
 fun isTooHot(temperature: Int) = temperature > 30
--- app/src/main/java/bootcamp/com/bootcampkotlin/MainActivity.kt
+++ app/src/main/java/bootcamp/com/bootcampkotlin/MainActivity.kt
@@ -1,7 +1,7 @@
 package bootcamp.com.bootcampkotlin
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
