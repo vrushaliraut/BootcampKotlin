54a6100e6b7f0924751ed2cedb190668199c3ac2
Vrushali <vrushali.raut@go-jek.com>
2018-07-22 18:12:01 +0530

    Add Inheritance example

--- app/src/main/java/bootcamp/com/bootcampkotlin/MainActivity.kt
+++ app/src/main/java/bootcamp/com/bootcampkotlin/MainActivity.kt
@@ -80,6 +80,23 @@ class MainActivity : AppCompatActivity() {
             return Triple(title,author,year)
         }
     }
-
-
 }
+
+open class XBook(val title: String, val author: String) {
+
+    private var currentPage = 1
+
+    open fun readPage() {
+        currentPage++
+    }
+}
+
+class eBook(title: String, author: String, var format: String = "text") : XBook(title, author) {
+
+    private var wordsRead = 0
+
+    override fun readPage() {
+        wordsRead = wordsRead + 250
+    }
+}
+
