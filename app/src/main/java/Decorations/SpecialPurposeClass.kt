cf847978eb0b5284e90b8205f761864b59d7ba17
Vrushali <vrushali.raut@go-jek.com>
2018-07-22 23:06:14 +0530

    Special purpose classes - Sealed,Object,enum

---
+++ app/src/main/java/Decorations/SpecialPurposeClass.kt
@@ -0,0 +1,34 @@
+package Decorations
+
+//Object class
+object Mobidick{
+    val author = "sdsd"
+    fun jump{
+
+    }
+}
+
+//Define class- you can give them property or methods
+enum class Color(val rgb : Int){
+    Red(0xFF000),
+    Green(0xFF000),
+    Blue(0xFF000)
+}
+
+//Sealed class - special class
+// It's a class that can be subclassed but only inside a filed which is declared
+//if you try to subclass it in different file, you will get an error
+
+//** greate for returning success or error from API Network
+sealed class Seal
+
+//oth type of Seal - as all are in same class kotline file know it's Subclass
+class SeaLion : Seal()
+class Walrus : Seal()
+
+fun matchSeal(seal: Seal):String{
+    return when(seal){
+        is SeaLion -> "Sealion"
+        is  Walrus -> "walrus"
+    }
+}
