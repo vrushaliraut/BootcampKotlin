d2e0b3449d1b30371431c3686b7fd151a936602e
Vrushali <vrushali.raut@go-jek.com>
2018-08-01 16:16:18 +0530

    Comment Inline Functions

--- app/src/main/java/Aquarium5/InlineFunctions.kt
+++ app/src/main/java/Aquarium5/InlineFunctions.kt
@@ -1,7 +1,5 @@
 package Aquarium5
 
-import android.annotation.SuppressLint
-import android.util.Log
 import java.util.function.Function
 
 fun InlineFishExample() {
@@ -10,18 +8,21 @@ fun InlineFishExample() {
     /* One problem with this functions is
         Every-time time you call this function it create new object of high order
         Lambdas are Objects
-        Lambda expression is instance of function interface, which is itself a subtype of object
+        Lambda expression is instance of function interface, which is itself a subtype of object */
 
 
-    * */
-    myInlinewith(fish.name, @SuppressLint("NewApi")
-    object : Function<String,Unit>){
-        override fun invoke(name: String){
+    //TODO fix  - Inline functions example
+    /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) myInlinewith(
+            fish.name,
+            block = @SuppressLint("NewApi")
+            object : Function<String,Unit>
+    ) {
+        fun invoke(name: String){
             Log.d("Actual High order: ",""+name.capitalize())
         }
-
-    }
+    }*/
 }
 inline fun myInlinewith(name: String, block: Function<String, Unit>, function: () -> Unit){
-    name.block()
+    name
+    block
 }
